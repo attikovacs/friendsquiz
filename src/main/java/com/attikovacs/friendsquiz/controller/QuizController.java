38d234970715da75e1610c8ede9e26fee7df9812
attikovacs <attikovacs@gmail.com>
2017-12-10 18:25:33 +0100

    Introduced logging using Logback (Spring Boot default) with configuration in application.properties file.

--- src/main/java/com/attikovacs/friendsquiz/controller/QuizController.java
+++ src/main/java/com/attikovacs/friendsquiz/controller/QuizController.java
@@ -1,14 +1,19 @@
 package com.attikovacs.friendsquiz.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.attikovacs.friendsquiz.model.Question;
 import com.attikovacs.friendsquiz.services.QuizService;
 
 @Controller
 public class QuizController {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	private QuizService quizService;
 
 	public QuizController(QuizService quizService) {
@@ -17,8 +22,9 @@ public class QuizController {
 	
 	@RequestMapping(value = {"", "/"})
 	public String getQuestion(Model model) {
-		model.addAttribute("question", quizService.getQuestion());
-		System.out.println(System.currentTimeMillis() + " new question");
+		Question question = quizService.getQuestion();
+		model.addAttribute("question", question);
+		logger.info(question.getQuestion() + " - " + question.getAnswers().toString() + " - correct answer: " + question.getCorrectIndex());
 		return "friendsquiz";
 	}
 	
